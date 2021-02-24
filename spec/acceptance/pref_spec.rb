c5555a3394fef001105a7076faec9ea3dcc81fdd
Romain Tartière <romain@blogreen.org>
2021-02-24 09:17:45 -1000

    Add acceptance tests

---
+++ spec/acceptance/pref_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper_acceptance'
+
+describe 'firefox::pref class' do
+  it 'works idempotently with no errors' do
+    pp = <<~MANIFEST
+      firefox::pref { 'browser.urlbar.trimURLs':
+        value => false,
+      }
+    MANIFEST
+
+    apply_manifest(pp, catch_failures: true)
+    apply_manifest(pp, catch_changes: true)
+  end
+end
