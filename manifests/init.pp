0ca28908221b19751e84fd0504199d75a00419ed
Romain Tartière <romain@blogreen.org>
2021-04-10 19:16:20 -1000

    Add support for policies
    [Policies](https://github.com/mozilla/policy-templates/releases) allow a
    wide range of personalization.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,16 @@
 # @summary Manage the Firefox web browser
 #
 # @param directory Base directory of Firefox's installation
-# @param owner User owning the preferences configuration file
-# @param group Group owning the preferences configuration file
+# @param owner User owning the configuration files
+# @param group Group owning the configuration files
 # @param managed_directories A list of directories to manage
 # @param manage_package Manage the firefox package on the system
 # @param package The name of the firefox package
 # @param package_ensure Value of the ensure parameter of the firefox package
 # @param package_provider Value of the provider parameter of the firefox package
 # @param preferences_file Path to Firefox's preferences configuration file
+# @param policies_file Path to Firefox's policies file
+# @param policies The policies to deploy, see https://github.com/mozilla/policy-templates for the structure of this Hash
 class firefox (
   Stdlib::Absolutepath        $directory,
   Optional[String[1]]         $owner,
@@ -19,13 +21,18 @@ class firefox (
   Boolean                     $manage_package   = true,
   Optional[String[1]]         $package_provider = undef,
   Stdlib::Absolutepath        $preferences_file = "${directory}/browser/defaults/preferences/00-puppet-preferences.js",
+  Stdlib::Absolutepath        $policies_file    = "${directory}/distribution/policies.json",
+  Hash                        $policies         = {},
 ) {
   file { $managed_directories.map |$d| { "${directory}/${d}" }:
     ensure => directory,
     owner  => $owner,
     group  => $group,
     mode   => '0755',
-    before => Concat[$preferences_file],
+    before => [
+      Concat[$preferences_file],
+      File[$policies_file],
+    ],
   }
 
   concat { $preferences_file:
@@ -33,6 +40,16 @@ class firefox (
     owner  => $owner,
     group  => $group,
     mode   => '0644',
+  }
+
+  file { $policies_file:
+    ensure  => file,
+    owner   => $owner,
+    group   => $group,
+    mode    => '0644',
+    content => {
+      policies => $policies,
+    }.to_json(),
   }
 
   if $manage_package {
