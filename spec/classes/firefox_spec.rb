8832cda5caf608bb65973d373d7d6d6335380428
Romain Tartière <romain@blogreen.org>
2024-06-20 09:31:24 -1000

    Add support for Debian 12

--- spec/classes/firefox_spec.rb
+++ spec/classes/firefox_spec.rb
@@ -9,7 +9,7 @@ describe 'firefox' do
 
       it { is_expected.to compile.with_all_deps }
 
-      if %w[debian-10-x86_64 debian-11-x86_64].include?(os)
+      if %w[debian-10-x86_64 debian-11-x86_64 debian-12-x86_64].include?(os)
         it { is_expected.to contain_package('firefox-esr') }
       else
         it { is_expected.to contain_package('firefox') }
